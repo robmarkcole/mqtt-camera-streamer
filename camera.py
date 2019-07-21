3f9007de671ec1e4eabbd64ca9c629bbeeb31e0b
robmarkcole <robmarkcole@gmail.com>
2019-07-21 07:24:34 +0100

    Make camera live

--- camera.py
+++ camera.py
@@ -1,8 +1,10 @@
 """
 Capture frames from a camera using openCV and publish on an MQTT topic.
 """
+import datetime
 import time
 import io
+import sys
 import mqtt as mqtt
 
 from PIL import Image
@@ -13,6 +15,8 @@ MQTT_PORT = 1883
 MQTT_TOPIC = "homie/mac_webcam/capture"
 MQTT_QOS = 1
 
+DATETIME_STR_FORMAT = "%Y-%m-%d_%H:%M:%S.%f"
+FPS = 1  # The frames per second to stream
 VIDEO_SOURCE = 0
 
 
@@ -25,23 +29,29 @@ def main():
 
     # Open camera and capture frame
     cap = cv2.VideoCapture(VIDEO_SOURCE)
-    time.sleep(3)  # Webcam light should come on
-    _, np_array = cap.read()  # capture a frame
-    print("Frame captured!")
-    imgRGB = cv2.cvtColor(np_array, cv2.COLOR_BGR2RGB)  # Convert to RGB
-    image = Image.fromarray(imgRGB)  #  PIL image
-    # image.show() # for debugging only
+    time.sleep(2)  # Webcam light should come on
+    try:
+        while True:
+            _, np_array = cap.read()  # capture a frame
+            np_array_RGB = cv2.cvtColor(np_array, cv2.COLOR_BGR2RGB)  # Convert to RGB
+            image = Image.fromarray(np_array_RGB)  #  PIL image
+            # image.show() # for debugging only
 
-    ## Get the bytearray
-    imgByteArr = io.BytesIO()
-    image.save(imgByteArr, "PNG")
-    imgByteArr = imgByteArr.getvalue()
+            ## Get the bytearray
+            imgByteArr = io.BytesIO()
+            image.save(imgByteArr, "PNG")
+            imgByteArr = imgByteArr.getvalue()
 
-    # Publish
-    client.publish(MQTT_TOPIC, imgByteArr, qos=MQTT_QOS)
-    print(f"published frame on topic: {MQTT_TOPIC}")
-    print("completed")
-    cap.release()
+            # Publish
+            client.publish(MQTT_TOPIC, imgByteArr, qos=MQTT_QOS)
+            date_string = datetime.datetime.now().strftime(DATETIME_STR_FORMAT)
+            print(f"published frame on topic: {MQTT_TOPIC} at {date_string}")
+            time.sleep(1 / FPS)
+
+    except KeyboardInterrupt:
+        print("Closing camera")
+        cap.release()
+        sys.exit()
 
 
 if __name__ == "__main__":
